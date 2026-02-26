90bde89cfac04cba48e362f01631d7fe3b6da5f9
Sawan Kuma Das <71059909+ProPhycient5@users.noreply.github.com>
2026-02-26 09:40:55 +0530

    rightAngledTrianglePattern

---
+++ StarPatternProblems/startPattern2.js
@@ -0,0 +1,18 @@
+
+function createStarPattern2(n){
+    for (let i = 0; i < n; i++){
+        let star = ""
+        for(let j = 0; j < i; j++){
+            star = star + " " + "*"
+        }
+        console.log(star)
+    }
+}
+
+
+createStarPattern2(5)
+
+//*
+//* *
+//* * *
+//* * * *
