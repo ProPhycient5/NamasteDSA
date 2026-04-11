7fd2a32b4f98833f6d669f22af607f3243a07d7c
Sawan Kuma Das <71059909+ProPhycient5@users.noreply.github.com>
2026-04-11 12:24:56 +0530

    single number

---
+++ Array/SingleNumber.js
@@ -0,0 +1,10 @@
+//https://leetcode.com/problems/single-number/description/
+
+var singleNumber = function(nums) {
+ let xor = 0;
+
+ for (let i = 0; i < nums.length; i++){
+     xor = xor ^ nums[i];
+ }
+ return xor;
+};
